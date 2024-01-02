c56d974193e58d4f2321074d2a59dd93a70c47a8
Ashley Yang <ashley.minghui@gmail.com>
2024-01-02 13:26:16 -0800

    Remove unnecessary fragment and fix space in TimelineEvent

--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -3,7 +3,7 @@ import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
 
 const TimelineEvent = (props) => {
-    return(
+    return (
         <section className="timeline-event">
             <h3 className="event-person">{props.person}</h3>
             <p className="event-status">{props.status}</p>
